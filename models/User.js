4ec9527c20118444c7b92dfdd21bcbde5319ad67
Kuldeep Kamboj <kuldeepk1979@gmail.com>
2018-02-09 00:37:56 +0530

    change fields

--- models/User.js
+++ models/User.js
@@ -1,8 +1,14 @@
 var mongoose = require('mongoose');
 var UserSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  password: String
+  firstName: { type: String },
+  lastName: { type: String },
+  email: { type: String },
+  password: { type: String },
+  dob: { type: Date, default: Date.now },
+  education: { type: String },
+  occupation: { type: String },
+  currentLocation: { type: String },
+  permLocation: { type: String }
 });
 mongoose.model('User', UserSchema);
 
